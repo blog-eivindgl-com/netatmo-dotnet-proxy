6d8219274a158c00956d32f2ba14d6b159d4f21b
Eivind Gussiås Løkseth <eloekset@msn.com>
2023-01-12 21:50:39 +0100

    Fix incorrect formatting of timezone offset

--- src/NetatmoProxy/NetatmoProxy.Services.Tests/PythonDateTimeFormatService_StrfFormatShould.cs
+++ src/NetatmoProxy/NetatmoProxy.Services.Tests/PythonDateTimeFormatService_StrfFormatShould.cs
@@ -7,7 +7,8 @@ namespace NetatmoProxy.Services.Tests
 {
     public class PythonDateTimeFormatService_StrfFormatShould
     {
-        private readonly DateTimeOffset _mockedNow = DateTimeOffset.ParseExact("2023-02-15 13:15:16.123", "yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+        private const string MockedTimeFormat = "yyyy-MM-dd HH:mm:ss.fff";
+        private DateTimeOffset _mockedNow = DateTimeOffset.ParseExact("2023-02-15 13:15:16.123", MockedTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
         private readonly Mock<INowService> _nowServiceMock;
         private readonly PythonDateTimeFormatService _service;
 
@@ -36,6 +37,23 @@ namespace NetatmoProxy.Services.Tests
 
             // Assert
             actualOutput.ShouldBe(expectedOutput, "Output as expected");
+        }
+
+        [Theory]
+        [InlineData(@"Europe/Oslo", "2023-02-15 13:15:16.123", "2023-02-15 14:15:16.123 046 3 +0100 CET")]
+        [InlineData(@"Europe/Tallinn", "2023-02-15 13:15:16.123", "2023-02-15 15:15:16.123 046 3 +0200 EET")]
+        [InlineData(@"America/Chicago", "2023-02-15 13:15:16.123", "2023-02-15 07:15:16.123 046 3 -0600 CST")]
+        public void ReturnCorrectTimeForSupportedTimeZones(string timezone, string utcNow, string expectedOutput)
+        {
+            // Arrange
+            string format = "%Y-%m-%d %H:%M:%S.%L %j %u %z %Z";
+            _mockedNow = DateTimeOffset.ParseExact(utcNow, MockedTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+
+            // Act
+            string actualOutput = _service.StrfTime(timezone, format);
+
+            // Assert
+            actualOutput.ShouldBe(expectedOutput);
         }
     }
 }
