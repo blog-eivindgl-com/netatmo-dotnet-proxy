f4cf0f15440d9d9dda1aad41bcbc6b78433b1654
Eivind Gussiås Løkseth <eivind@outlook.com>
2023-03-12 23:10:25 +0100

    Issue #26 - Return null if any exception occurs See if this fixes the OutOfRetries error on the PyPortal device.

--- src/NetatmoProxy/NetatmoProxy/Controllers/DisplayController.cs
+++ src/NetatmoProxy/NetatmoProxy/Controllers/DisplayController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using NetatmoProxy.Configuration;
 using NetatmoProxy.Model;
 using NetatmoProxy.Model.Netatmo;
@@ -11,12 +10,14 @@ namespace NetatmoProxy.Controllers
     [ApiController]
     public class DisplayController : ControllerBase
     {
+        private readonly ILogger<DisplayController> _logger;
         private readonly NetatmoApiConfig _config;
         private readonly INetatmoApiService _netatmoApiService;
         private readonly IDayNightService _dayNightService;
 
-        public DisplayController(NetatmoApiConfig config, INetatmoApiService netatmoApiService, IDayNightService dayNightService)
+        public DisplayController(ILogger<DisplayController> logger, NetatmoApiConfig config, INetatmoApiService netatmoApiService, IDayNightService dayNightService)
         {
+            _logger = logger;
             _config = config;
             _netatmoApiService = netatmoApiService;
             _dayNightService = dayNightService;
@@ -25,115 +26,124 @@ namespace NetatmoProxy.Controllers
         [HttpGet]
         public async Task<Display> Get()
         {
-            var stationData = await _netatmoApiService.GetStationsDataAsync(new Model.Netatmo.GetStationsDataRequest());
-
-            IEnumerable<Device> indoorModules = stationData?.Body?.Devices;
-            var outdoorTemperatureModules = new List<Module>();
-            var outdoorWindModules = new List<Module>();
-            var batteryIndicators = new List<BatteryIndicator>();
-
-            foreach (var indoorModule in indoorModules)
+            try
             {
-                outdoorTemperatureModules.AddRange(
-                    from m in indoorModule.Modules
-                    where _config.Modules.Contains(m.ModuleName, StringComparer.InvariantCultureIgnoreCase)
-                    && m.DataType.Contains("Temperature") && m.DashboardData != null
-                    select m
-                    );
-                outdoorWindModules.AddRange(
-                    from m in indoorModule.Modules
-                    where _config.Modules.Contains(m.ModuleName, StringComparer.InvariantCultureIgnoreCase)
-                    && m.DataType.Contains("Wind") && m.DashboardData != null
-                    select m
-                    );
-                batteryIndicators.AddRange(
-                    from m in indoorModule.Modules
-                    where _config.Modules.Contains(m.ModuleName, StringComparer.InvariantCultureIgnoreCase)
-                    select new BatteryIndicator
+                var stationData = await _netatmoApiService.GetStationsDataAsync(new Model.Netatmo.GetStationsDataRequest());
+
+                IEnumerable<Device> indoorModules = stationData?.Body?.Devices;
+                var outdoorTemperatureModules = new List<Module>();
+                var outdoorWindModules = new List<Module>();
+                var batteryIndicators = new List<BatteryIndicator>();
+
+                foreach (var indoorModule in indoorModules)
+                {
+                    outdoorTemperatureModules.AddRange(
+                        from m in indoorModule.Modules
+                        where _config.Modules.Contains(m.ModuleName, StringComparer.InvariantCultureIgnoreCase)
+                        && m.DataType.Contains("Temperature") && m.DashboardData != null
+                        select m
+                        );
+                    outdoorWindModules.AddRange(
+                        from m in indoorModule.Modules
+                        where _config.Modules.Contains(m.ModuleName, StringComparer.InvariantCultureIgnoreCase)
+                        && m.DataType.Contains("Wind") && m.DashboardData != null
+                        select m
+                        );
+                    batteryIndicators.AddRange(
+                        from m in indoorModule.Modules
+                        where _config.Modules.Contains(m.ModuleName, StringComparer.InvariantCultureIgnoreCase)
+                        select new BatteryIndicator
+                        {
+                            ModuleName = m.ModuleName,
+                            BatteryLevel = m.BatteryPercent
+                        });
+                }
+
+                // Exclude indoor modules not configured for display
+                indoorModules = (from d in indoorModules
+                                 where _config.Modules.Contains(d.ModuleName)
+                                 && d.DashboardData != null
+                                 select d).ToList();
+
+                var sunOrMoon = await _dayNightService.IsSunOrMoonAsync();
+
+                string FormatTemperature(decimal temperature)
+                {
+                    return $"{temperature.ToString("n1")}°C";
+                }
+
+                string FormatDateTime(int dateValue)
+                {
+                    return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(dateValue).ToString("dd.MM HH:mm");
+                }
+
+                string FormatWindStrength(decimal strenghInKmPerHour)
+                {
+                    return $"{(strenghInKmPerHour / 3.6m).ToString("n1")}m/s";
+                }
+
+                Widget CreateTemperatureWidget(string name, DashboardData data)
+                {
+                    return new Widget
                     {
-                        ModuleName = m.ModuleName,
-                        BatteryLevel = m.BatteryPercent
-                    });
-            }
+                        Type = "temperature",
+                        Description = name,
+                        Value = FormatTemperature(data.Temperature),
+                        Trend = data.TempTrend,
+                        MinValue = FormatTemperature(data.MinTemp),
+                        MinTime = FormatDateTime(data.DateMinTemp),
+                        MaxValue = FormatTemperature(data.MaxTemp),
+                        MaxTime = FormatDateTime(data.DateMaxTemp)
+                    };
+                }
 
-            // Exclude indoor modules not configured for display
-            indoorModules = (from d in indoorModules
-                            where _config.Modules.Contains(d.ModuleName)
-                            && d.DashboardData != null
-                            select d).ToList();
-
-            var sunOrMoon = await _dayNightService.IsSunOrMoonAsync();
-
-            string FormatTemperature(decimal temperature)
-            {
-                return $"{temperature.ToString("n1")}°C";
-            }
-
-            string FormatDateTime(int dateValue)
-            {
-                return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(dateValue).ToString("dd.MM HH:mm");
-            }
-
-            string FormatWindStrength(decimal strenghInKmPerHour)
-            {
-                return $"{(strenghInKmPerHour / 3.6m).ToString("n1")}m/s";
-            }
-
-            Widget CreateTemperatureWidget(string name, DashboardData data)
-            {
-                return new Widget
+                Widget CreateHumidityWidget(string name, DashboardData data, int batteryPercent)
                 {
-                    Type = "temperature",
-                    Description = name,
-                    Value = FormatTemperature(data.Temperature),
-                    Trend = data.TempTrend,
-                    MinValue = FormatTemperature(data.MinTemp),
-                    MinTime = FormatDateTime(data.DateMinTemp),
-                    MaxValue = FormatTemperature(data.MaxTemp),
-                    MaxTime = FormatDateTime(data.DateMaxTemp)
+                    return new Widget
+                    {
+                        Type = "humidity",
+                        Description = name,
+                        Value = data.Humidity.ToString(),
+                        OutTemp = data.Temperature,
+                        BatteryLevel = batteryPercent,
+                        SunOrMoon = sunOrMoon,
+                        BatteryIndicators = batteryIndicators.ToArray()
+                    };
+                }
+
+                Widget CreateWindWidget(string name, DashboardData data, int batteryPercent)
+                {
+                    return new Widget
+                    {
+                        Type = "wind",
+                        Description = name,
+                        Value = "Vind".Equals(name) ? FormatWindStrength(data.WindStrength) : FormatWindStrength(data.GustStrength),
+                        Angle = "Vind".Equals(name) ? data.WindAngle.ToString() : data.GustAngle.ToString(),
+                        MaxValue = FormatWindStrength(data.MaxWindStrength),
+                        MaxAngle = data.MaxWindAngle.ToString(),
+                        MaxTime = FormatDateTime(data.DateMaxWind),
+                        BatteryLevel = batteryPercent
+                    };
+                }
+
+                var widgets = new List<Widget>();
+                widgets.AddRange(indoorModules.Select(m => CreateTemperatureWidget(m.ModuleName, m.DashboardData)));
+                widgets.AddRange(outdoorTemperatureModules.Select(m => CreateTemperatureWidget(m.ModuleName, m.DashboardData)));
+                widgets.AddRange(outdoorTemperatureModules.Select(m => CreateHumidityWidget(m.ModuleName, m.DashboardData, m.BatteryPercent)));
+                widgets.AddRange(outdoorWindModules.Select(m => CreateWindWidget("Vind", m.DashboardData, m.BatteryPercent)));
+                widgets.AddRange(outdoorWindModules.Select(m => CreateWindWidget("Kast", m.DashboardData, m.BatteryPercent)));
+
+                return new Display
+                {
+                    Widgets = widgets
                 };
             }
-
-            Widget CreateHumidityWidget(string name, DashboardData data, int batteryPercent)
+            catch(Exception ex)
             {
-                return new Widget
-                {
-                    Type = "humidity",
-                    Description = name,
-                    Value = data.Humidity.ToString(),
-                    OutTemp = data.Temperature,
-                    BatteryLevel = batteryPercent,
-                    SunOrMoon = sunOrMoon,
-                    BatteryIndicators = batteryIndicators.ToArray()
-                };
+                _logger.LogError(ex, "An error occured calling any of the APIs for the Display data");
             }
 
-            Widget CreateWindWidget(string name, DashboardData data, int batteryPercent)
-            {
-                return new Widget
-                {
-                    Type = "wind",
-                    Description = name,
-                    Value = "Vind".Equals(name) ? FormatWindStrength(data.WindStrength) : FormatWindStrength(data.GustStrength),
-                    Angle = "Vind".Equals(name) ? data.WindAngle.ToString() : data.GustAngle.ToString(),
-                    MaxValue = FormatWindStrength(data.MaxWindStrength),
-                    MaxAngle = data.MaxWindAngle.ToString(),
-                    MaxTime = FormatDateTime(data.DateMaxWind),
-                    BatteryLevel = batteryPercent
-                };
-            }
-
-            var widgets = new List<Widget>();
-            widgets.AddRange(indoorModules.Select(m => CreateTemperatureWidget(m.ModuleName, m.DashboardData)));
-            widgets.AddRange(outdoorTemperatureModules.Select(m => CreateTemperatureWidget(m.ModuleName, m.DashboardData)));
-            widgets.AddRange(outdoorTemperatureModules.Select(m => CreateHumidityWidget(m.ModuleName, m.DashboardData, m.BatteryPercent)));
-            widgets.AddRange(outdoorWindModules.Select(m => CreateWindWidget("Vind", m.DashboardData, m.BatteryPercent)));
-            widgets.AddRange(outdoorWindModules.Select(m => CreateWindWidget("Kast", m.DashboardData, m.BatteryPercent)));
-
-            return new Display
-            {
-                Widgets = widgets
-            };
+            return null;
         }
     }
 }
