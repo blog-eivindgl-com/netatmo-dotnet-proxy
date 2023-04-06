1166ae03f001cfef94065dc0b3006dc5bf54c7c3
Eivind Gussiås Løkseth <eivind@outlook.com>
2023-04-06 19:27:48 +0200

    Add Trace API operation to trace PyPortal program using Application Insights

---
+++ src/NetatmoProxy/NetatmoProxy/Controllers/TraceController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetatmoProxy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class TraceController : ControllerBase
+    {
+        private readonly ILogger<TraceController> _logger;
+
+        public TraceController(ILogger<TraceController> logger) 
+        { 
+            _logger = logger;
+        }
+
+        [HttpPost("Information")]
+        public IActionResult Information([FromBody] string text)
+        {
+            _logger.LogInformation(text);
+            return Ok();
+        }
+
+        [HttpPost("Error")]
+        public IActionResult Error([FromBody] string error) 
+        {
+            _logger.LogError(error);
+            return Ok();
+        }
+    }
+}
